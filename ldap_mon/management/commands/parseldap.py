fdc8ca7166d3e30111f7351fbc04ace0dd46a891
Alex Morega <alex@grep.ro>
2012-12-07 12:18:03 +0200

    management command to fetch log entries

---
+++ ldap_mon/management/commands/parseldap.py
@@ -0,0 +1,15 @@
+from optparse import make_option
+from django.core.management.base import BaseCommand
+from ldap_mon.models import fetch_and_parse
+
+
+class Command(BaseCommand):
+    help = "Fetch new log entries from the database."
+    option_list = BaseCommand.option_list + (
+        make_option('-p', '--remove',
+                    action='store_true', dest='remove', default=False,
+                    help="Remove old log entries."),
+    )
+
+    def handle(self, *args, **options):
+        fetch_and_parse(options['remove'])
