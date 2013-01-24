de235334044f7c3b803fce0b795add3c2fc29e4b
Alex Morega <alex@grep.ro>
2013-01-24 18:03:25 +0200

    test fixtures next to test case

--- parser_test.py
+++ parser_test.py
@@ -1,4 +1,3 @@
-import unittest
 from datetime import datetime
 from nose.tools import assert_equal
 
@@ -7,6 +6,7 @@ def _log_fixture(time, messages):
     return [(time, msg) for msg in messages.strip().splitlines()]
 
 TIME = datetime(2013, 1, 27, 13, 34, 55)
+
 
 LOG_ONE_BIND = _log_fixture(TIME, """
 conn=1007 fd=18 ACCEPT from IP=127.0.0.1:36676 (IP=0.0.0.0:389)
@@ -17,6 +17,13 @@ conn=1007 op=3 UNBIND
 conn=1007 fd=18 closed
 """)
 
+
+def test_parse_one_bind_operation():
+    from logparser import parse
+    assert_equal(parse(LOG_ONE_BIND),
+                 [{'remote_addr': '127.0.0.1', 'uid': 'uzer', 'time': TIME}])
+
+
 LOG_INTERLEAVED_BINDS = _log_fixture(TIME, """
 conn=1007 fd=18 ACCEPT from IP=127.0.0.1:36676 (IP=0.0.0.0:389)
 conn=1008 fd=18 ACCEPT from IP=127.0.0.2:36676 (IP=0.0.0.0:389)
@@ -25,16 +32,8 @@ conn=1008 op=2 BIND dn="uid=uz2,ou=Users,o=EIONET,l=Europe" mech=SIMPLE ssf=0
 """)
 
 
-def parse(*args, **kwargs):
-    from logparser import parse
-    return parse(*args, **kwargs)
-
-
-def test_parse_one_bind_operation():
-    assert_equal(parse(LOG_ONE_BIND),
-                 [{'remote_addr': '127.0.0.1', 'uid': 'uzer', 'time': TIME}])
-
 def test_parse_two_interleaved_binds():
+    from logparser import parse
     assert_equal(parse(LOG_INTERLEAVED_BINDS), [
         {'remote_addr': '127.0.0.1', 'uid': 'uz1', 'time': TIME},
         {'remote_addr': '127.0.0.2', 'uid': 'uz2', 'time': TIME},
