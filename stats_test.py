1a3649e3c76dc45bf59829358a8b3b13f60c51a5
Alex Morega <alex@grep.ro>
2013-02-11 16:19:31 +0200

    extract session creation

--- stats_test.py
+++ stats_test.py
@@ -6,10 +6,15 @@ from testing_utils import create_memory_db
 TIME = datetime(2013, 1, 27, 13, 34, 55)
 
 
-def test_event_updates_last_login():
+def _create_session():
     import logstats
     Session = create_memory_db(logstats.Model.metadata)
-    session = Session()
+    return Session()
+
+
+def test_event_updates_last_login():
+    import logstats
+    session = _create_session()
     logstats.update_stats(session, [{'uid': 'uzer', 'time': TIME}])
     persons = session.query(logstats.Person).all()
     assert_equal([(p.uid, p.last_login) for p in persons], [('uzer', TIME)])
